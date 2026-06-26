1bd94a0e62d53769f6f78f08c03bddbdcfcb2e1b
Henry Seeger <hiseeger@gmail.com>
2026-06-25 22:05:05 -0400

    Created file upload system with a 'clear files' button

--- FACS_3D_Plotting.py
+++ FACS_3D_Plotting.py
@@ -2,21 +2,27 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-uploaded_files = st.file_uploader("Upload FACS Data Folder Here", accept_multiple_files=True, type = "fcs")
+if "uploader_key" not in st.session_state:
+  st.session_state.uploader_key = 0
+
+uploaded_files = st.file_uploader("Upload FACS Data Folder Here", accept_multiple_files = True, type = ["fcs"], key=f"uploader_key_{st.session_state.uploader_key}")
+
+def clear_files_on_click():
+  if uploaded_files is not None:
+    st.session_state.uploader_key += 1
+
+clear_files = st.button("Clear Uploaded Files", on_click = clear_files_on_click)#, help = "Warning: Clearing the downloads automatically requires rerunning the page")
 
 files = []
 
-for i, uploaded_file in enumerate(uploaded_files):
-  files.append(pd.read_csv(uploaded_file))
-
-file_names_text = st.text("")
+# for i, uploaded_file in enumerate(uploaded_files):
+#   files.append(pd.read_csv(uploaded_file))
 
 files_selected = st.multiselect("Users", [file.name for file in uploaded_files], default=[])
 
-if uploaded_files is not None and len(files) > 0:
-  file_names_text.text([file.name for file in uploaded_files])
-  fig = px.scatter_3d(files[0], x=' BV421-A', y=' Alexa Fluor 647-A', z=' GFP-A')
-  fig.update_xaxes(title_text = "SREBP1")
-  fig.update_yaxes(title_text = "BMRF1")
-  # fig.update_zaxes(title_text = "SREBP1")
-  st.plotly_chart(fig)
+# if uploaded_files is not None:# and len(files) > 0:
+  # fig = px.scatter_3d(files[0], x=' BV421-A', y=' Alexa Fluor 647-A', z=' GFP-A')
+  # fig.update_xaxes(title_text = "SREBP1")
+  # fig.update_yaxes(title_text = "BMRF1")
+  # # fig.update_zaxes(title_text = "SREBP1")
+  # st.plotly_chart(fig)
