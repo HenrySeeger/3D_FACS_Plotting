1a42279c20ec1269bbbff59a65aae9873bd13769
Henry Seeger <hiseeger@gmail.com>
2026-06-29 13:51:30 -0400

    Minor changes

--- FACS_3D_Plotting.py
+++ FACS_3D_Plotting.py
@@ -11,6 +11,11 @@ def clear_files_on_click():
   if uploaded_files is not None:
     st.session_state.uploader_key += 1
 
+def fcs_to_csv(fcs_file):
+  print("fcs_to_csv")
+
+files_csvs = {file.name : fcs_to_csv(file) for file in uploaded_files} # Needs to be file.keyword, not just file
+
 clear_files = st.button("Clear Uploaded Files", on_click = clear_files_on_click)#, help = "Warning: Clearing the downloads automatically requires rerunning the page")
 
 files = []
@@ -18,11 +23,14 @@ files = []
 # for i, uploaded_file in enumerate(uploaded_files):
 #   files.append(pd.read_csv(uploaded_file))
 
-files_selected = st.multiselect("Users", [file.name for file in uploaded_files], default=[])
-
 # if uploaded_files is not None:# and len(files) > 0:
   # fig = px.scatter_3d(files[0], x=' BV421-A', y=' Alexa Fluor 647-A', z=' GFP-A')
   # fig.update_xaxes(title_text = "SREBP1")
   # fig.update_yaxes(title_text = "BMRF1")
   # # fig.update_zaxes(title_text = "SREBP1")
   # st.plotly_chart(fig)
+
+files_selected = st.multiselect("Users", [file.name for file in uploaded_files if "._" not in file.name], default=[])
+
+# if len(files_selected) == 3:
+#   print("plot stuff")
