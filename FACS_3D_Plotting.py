0eedbea82672d33a8af1647faa797e1f2de12414
Henry Seeger <hiseeger@gmail.com>
2026-07-01 14:40:47 -0400

    Brought in radio_buttons.py and an initial attempt at implementation of the radio buttons, successfully implemented fcs->dataframe

--- FACS_3D_Plotting.py
+++ FACS_3D_Plotting.py
@@ -1,36 +1,46 @@
+import io
+import flowio
 import numpy as np
 import pandas as pd
 import streamlit as st
 from fcsparser import parse
+import radio_buttons as rad
 import plotly.express as px
 
-if "uploader_key" not in st.session_state:
-  st.session_state.uploader_key = 0
+st.session_state.setdefault("facs_dataframes", [])
+st.session_state.setdefault("uploader_key", 0)
 
-uploaded_files = st.file_uploader("Upload FACS Data Folder Here", accept_multiple_files = True, type = ["fcs"], key=f"uploader_key_{st.session_state.uploader_key}")
 
 variables = {}
+
+@st.cache_data
+def load_fcs(uploaded_file_bytes):
+  flow = flowio.FlowData(io.BytesIO(uploaded_file_bytes))
+  return pd.DataFrame(flow.as_array(), columns = flow.pnn_labels), flow.text
+
+def load_all_fcs():
+  files = st.session_state[f"uploader_key_{st.session_state.uploader_key}"]
+  if files:
+    for file in [file for file in files if "._" not in file.name]:
+      df, metadata = load_fcs(file.getvalue())
+      st.session_state.facs_dataframes.append({"name" : file.name, "data" : df, "metadata" : metadata})
+  else:
+    st.session_state.facs_dataframes = []
 
 def clear_files_on_click():
   if uploaded_files is not None:
     st.session_state.uploader_key += 1
 
-def fcs_to_csv(fcs_file):
-  meta, data = parse(fcs_file.getvalue())
-  data.to_csv("sample.csv", index = False)
-  # print("fcs_to_csv")
-
-# files_csvs = {file.name : fcs_to_csv(file) for file in uploaded_files} # Needs to be file.keyword, not just file (pull byte data or something like that?)
+uploaded_files = st.file_uploader("Upload FCS file", type = ["fcs"], key=f"uploader_key_{st.session_state.uploader_key}", accept_multiple_files = True, on_change = load_all_fcs)
 
 clear_files = st.button("Clear Uploaded Files", on_click = clear_files_on_click) #, help = "Warning: Clearing the downloads automatically requires rerunning the page")
-
-files = []
 
 # for i, uploaded_file in enumerate(uploaded_files):
 #   files.append(pd.read_csv(uploaded_file))
 
+files_selected = st.multiselect("Files", [file.name[:file.name.rfind(".")] for file in uploaded_files if "._" not in file.name], default=[])
+
 if uploaded_files is not None:# and len(files) > 0:
-  st.button("Create csv", on_click = fcs_to_csv, args = (uploaded_files[0],))
   st.text(f"{[file.name for file in uploaded_files]}")
   # fig = px.scatter_3d(files[0], x=' BV421-A', y=' Alexa Fluor 647-A', z=' GFP-A')
   # fig.update_xaxes(title_text = "SREBP1")
@@ -38,7 +48,8 @@ if uploaded_files is not None:# and len(files) > 0:
   # # fig.update_zaxes(title_text = "SREBP1")
   # st.plotly_chart(fig)
 
-files_selected = st.multiselect("Files", [file.name for file in uploaded_files if "._" not in file.name], default=[])
+if len(st.session_state.facs_dataframes) > 0:
+  rad.radio_buttons_exclusive(row_labels = list(st.session_state.facs_dataframes[0]["data"].keys()), col_headers = ["x-axis", "y-axis", "z-axis"])
 
 # if len(files_selected) == 3:
 #   print("plot stuff")
---
+++ radio_buttons.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+def radio_buttons_exclusive(row_labels = ["one", "two", "three"], col_headers = [1, 2, 3]):
+  horizontal = len(col_headers) > len(row_labels)
+  
+  var_names = row_labels if horizontal else col_headers
+  var_names = [str(val) for val in var_names] # var_names is used for the label arg in st.radio() which requires a string input
+
+  button_options = col_headers if horizontal else row_labels
+
+  for i, default_val in enumerate(button_options):
+    key = f"rad{i}"
+    if key not in st.session_state:
+      st.session_state[key] = default_val
+
+  cols = var_names if horizontal else st.columns(len(col_headers), border = False)
+
+  def rad_change(changed):
+    for i in [i for i in range(len(var_names)) if i != changed]:
+      if st.session_state[f"rad{changed}"] == st.session_state[f"rad{i}"]:
+        available_indices = list(set(range(len(button_options))) ^ {button_options.index(st.session_state[f"rad{j}"]) for j in range(len(var_names)) if j != i}) # The available indices are the indices not currently being used (all indices xor in-use indices)
+        st.session_state[f"rad{i}"] = button_options[available_indices[0]]
+        break
+
+  for i, col in enumerate(cols):
+    try:
+      with col:
+        st.radio(label = var_names[i], options = button_options, horizontal = horizontal, key = f"rad{i}", on_change = rad_change, args = (i,))
+    except:
+      st.radio(label = var_names[i], options = button_options, horizontal = horizontal, key = f"rad{i}", on_change = rad_change, args = (i,))
+
+radio_buttons_exclusive()
