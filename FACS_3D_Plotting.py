17b2c75927ce2e6e739e3626cb35247f25a32162
Henry Seeger <hiseeger@gmail.com>
2026-07-03 15:29:45 -0400

    Implemented the dropdowns. Their available variables are determined by the selected files, not uploaded files and are disabled without selected files. 'No selection' is an option. The file selector was changed as well, it displays the same text, but the output is now a list of the selected files' dataframes as found in the session state (a dict containing file name, data, and metadata).

--- FACS_3D_Plotting.py
+++ FACS_3D_Plotting.py
@@ -37,19 +37,28 @@ clear_files = st.button("Clear Uploaded Files", on_click = clear_files_on_click)
 # for i, uploaded_file in enumerate(uploaded_files):
 #   files.append(pd.read_csv(uploaded_file))
 
-files_selected = st.multiselect("Files", [file.name[:file.name.rfind(".")] for file in uploaded_files if "._" not in file.name], default=[])
+def files_selected_formatting(file):
+  return file["name"][:file["name"].rfind(".")]
 
-if uploaded_files is not None:# and len(files) > 0:
-  st.text(f"{[file.name for file in uploaded_files]}")
+files_selected = st.multiselect(label = "Files", options = [file for file in st.session_state.facs_dataframes if "._" not in file["name"]], format_func = files_selected_formatting, default = [], placeholder = "No files selected")
+
+# if uploaded_files is not None:# and len(files) > 0:
+#   st.text(f"{[file.name for file in uploaded_files]}")
   # fig = px.scatter_3d(files[0], x=' BV421-A', y=' Alexa Fluor 647-A', z=' GFP-A')
   # fig.update_xaxes(title_text = "SREBP1")
   # fig.update_yaxes(title_text = "BMRF1")
   # # fig.update_zaxes(title_text = "SREBP1")
   # st.plotly_chart(fig)
 
+selectbox_columns = st.columns(3)
+
+for i, column in enumerate(selectbox_columns): # [st.session_state.facs_dataframes[file_name + ".fcs"] for file_name in files_selected]
+  with column:
+    st.selectbox(label = ["x-axis", "y-axis", "z-axis"][i], options = ["No Selection"] + list(set(key for df in files_selected for key in df["data"].keys())), key = f"axis{i}", disabled = len(files_selected) == 0)
+
 # Should make three inter-connected dropdowns instead of using radio buttons
 # if len(st.session_state.facs_dataframes) > 0:
-#   rad.radio_buttons_exclusive(row_labels = list(set(key for df in st.session_state.facs_dataframes for key in df["data"].keys())), col_headers = ["x-axis", "y-axis", "z-axis"])
+  # rad.radio_buttons_exclusive(row_labels = list(set(key for df in st.session_state.facs_dataframes for key in df["data"].keys())), col_headers = ["x-axis", "y-axis", "z-axis"])
 
 # if len(files_selected) == 3:
 #   print("plot stuff")
