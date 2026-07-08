6d0519dbb1c6d9852a20ef50d57226091929ef2e
Henry Seeger <hiseeger@gmail.com>
2026-07-08 12:33:07 -0400

    Fixed case sensitivity bug

--- quick_select.py
+++ quick_select.py
@@ -10,12 +10,12 @@ case_sensitive = st.toggle(label = "Filter is case-sensitive", value = True)
 filter_text = st.text_input("Input keyword here", value = "", key = "text_filter")
 
 if uploader and filter_text:
-  st.text(f"{len([file for file in uploader if (filter_text if case_sensitive else filter_text.lower()) in (file.name if case_sensitive else filter_text.lower())])} files selected")
+  st.text(f"{len([file for file in uploader if (filter_text if case_sensitive else filter_text.lower()) in (file.name if case_sensitive else file.name.lower())])} files selected")
   # Create an in-memory ZIP file
   zip_buffer = io.BytesIO()
 
   with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
-    for uploaded_file in [file for file in uploader if (filter_text if case_sensitive else filter_text.lower()) in (file.name if case_sensitive else filter_text.lower())]:
+    for uploaded_file in [file for file in uploader if (filter_text if case_sensitive else filter_text.lower()) in (file.name if case_sensitive else file.name.lower())]:
       zip_file.writestr(
         uploaded_file.name,     # Filename inside the ZIP
         uploaded_file.getvalue() # File contents
